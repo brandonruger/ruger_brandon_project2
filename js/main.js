b2696e8a3eb61740fda539d185483684994f4e1d
r02548 <bruger@fullsail.edu>
2013-08-15 16:07:31 -0400

    Set up JS to save data in local storage.

--- js/main.js
+++ js/main.js
@@ -12,17 +12,54 @@ window.addEventListener("DOMContentLoaded", function(){
     
     
     
+    //Create select field element and populuate it with options.
+    function makeFleaMedOptions() {
+        var formTag = document.getElementsByTagName("form"); // formTag is an array of all the form tags
+        var chooseList = $('select');
+        var makeSelect = document.createElement('select');
+        makeSelect.setAttribute("id", "fleaRx");
+        for (var i=0, j=fleaMedication.length; i<j; i++) {
+            var createOption = document.createElement('option');
+            var optionText = fleaMedication[i];
+            createOption.setAttribute("value", optionText);
+            createOption.innerHTML = optionText;
+            makeSelect.appendChild(createOption);
+        }
+        chooseList.appendChild(makeSelect);
+    }
+    
+    //Create function to submit data.
+    function submitData() {
+        var generateId =    Math.floor(Math.random()*100000001);
+        //Gather up all our form field values and store in an object.
+        //Object properties contain array with the form label and input value.
+        var itemList            = {};
+            itemList.fleaRx     = ["Flea Rx:", $('fleaRx').value];
+            itemList.petname    = ["Pet Name:", $('petname').value];
+            itemList.petage     = ["Pet Age:", $('petage').value];
+            itemList.pettype    = ["Pet Type:", $('pettype').value];
+            //itemList.flea       = ["Flea:", fleaValue];
+            //itemList.heartworm  = ["Heartworm:", heartwormValue];
+            //itemList.other      = ["Other:", otherValue];
+            itemList.date       = ["Date:", $('date').value];
+            itemList.range      = ["Range:", $('range').value];
+            itemList.note       = ["Note:", $('note').value];
+            
+            //Save data into Local Storage: use Stringify to convert object to a string:
+            localStorage.setItem(generateId, JSON.stringify(itemList));
+            alert("Reminder has been added!");
+    }
     
     //Variable Defaults
     var fleaMedication = ["--Type of Flea Medication--", "Topical", "Oral", "Spray-On"];
-    
+    makeFleaMedOptions();
     
     //Set Link & Submit Click Events
     
-    var displayData = $('displayData');
+    /*var displayData = $('displayData');
     displayData.addEventListener("click", getDataFromStorage);
     var clearData = $('clearData');
-    clearData.addEventListener("click", clearLocalStorage);
+    clearData.addEventListener("click", clearLocalStorage);*/
     var createButton = $('button');
     createButton.addEventListener("click", "submitData");
     
