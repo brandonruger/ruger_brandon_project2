2bab5c2ce561b87e6907b7b70e9f5d1728b56439
r02548 <bruger@fullsail.edu>
2013-08-15 17:46:16 -0400

    set up toggle form to turn form off

--- js/main.js
+++ js/main.js
@@ -47,7 +47,25 @@ window.addEventListener("DOMContentLoaded", function(){
         }
     }
     
-    
+    function toggleForm(n) {
+        switch (n) {
+            case "on":
+                $('addReminderForm').style.display = "none";
+                $('clearData').style.display = "inline";
+                $('displayData').style.display = "none";
+                $('addNewReminder').style.display = "inline";
+                break;
+            case "off":
+                $('addReminderForm').style.display = "block";
+                $('clearData').style.display = "inline";
+                $('displayData').style.display = "inline";
+                $('addNewReminder').style.display = "none";
+                $('itemList').style.display = "none";
+                break;
+            default:
+                return false;
+        }
+    }
     
     //Create function to submit data.
     function submitData() {
@@ -72,6 +90,34 @@ window.addEventListener("DOMContentLoaded", function(){
             alert("Reminder has been added!");
     }
     
+    function getDataFromStorage() {
+        toggleForm("on");
+        //Write Data from local storage to the browser
+        var createDiv = document.createElement('div');
+        createDiv.setAttribute("id", "items");
+        var createList = document.createElement('ul');
+        createDiv.appendChild(createList);
+        document.body.appendChild(createDiv);
+        $('items').style.display = "display";
+        for (var i=0; i<localStorage.length; i++) {
+            var createLi = document.createElement('li');
+            createList.appendChild(createLi);
+            var key = localStorage.key(i);
+            var value = localStorage.getItem(key);
+            //Convert string from local storage back to an object.
+            var obj = JSON.parse(value);
+            var makeSubList = document.createElement('ul');
+            createLi.appendChild(makeSubList);
+            for (var n in obj) {
+                var makeSubli = document.createElement('li');
+                makeSubList.appendChild(makeSubli);
+                var optSubText = obj[n][0] + " " + obj[n][1];
+                makeSubList.innerHTML = optSubText;
+            }
+        }
+        
+    }
+    
     //Variable Defaults
     var fleaMedication = ["--Type of Flea Medication--", "Topical", "Oral", "Spray-On"];
     var fleaCheckBox;
@@ -79,9 +125,9 @@ window.addEventListener("DOMContentLoaded", function(){
     
     //Set Link & Submit Click Events
     
-    /*var displayData = $('displayData');
+    var displayData = $('displayData');
     displayData.addEventListener("click", getDataFromStorage);
-    var clearData = $('clearData');
+    /*var clearData = $('clearData');
     clearData.addEventListener("click", clearLocalStorage);*/
     var createButton = $('button');
     createButton.addEventListener("click", "submitData");
